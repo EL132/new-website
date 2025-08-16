b4eb12efc9278261482db63854304a84e8c56558
EL132 <elias.lind6@gmail.com>
2025-08-16 14:42:34 -0400

    Update work experience

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -13,8 +13,10 @@ function Work () {
                 <h1 className='title-font'>WORK</h1>
             </div>
             <div className='work-container'>
-                <WorkBox company='Toyota' title='Supply Chain Engineering Intern' time='August 2023 - December 2023' website='https://www.toyota.com/'/>
                 <WorkBox company='Deloitte' title='Business Technology Solutions Summer Scholar' time='June 2024 - August 2024' website='https://www2.deloitte.com/us/en.html'/>
+                <WorkBox company='Google' title='Associate Product Management Intern' time='May 2025 - August 2025' website='https://www.google.com/'/>
+                <WorkBox company='Amazon' title='Software Development Engineering Co-op' time='August 2025 - December 2025' website='https://www.amazon.com/'/>
+                <WorkBox company='Toyota' title='Supply Chain Engineering Intern' time='August 2023 - December 2023' website='https://www.toyota.com/'/>
                 <WorkBox company='KnowAthens' title='Web Developer' time='September 2022 - May 2023' website='https://www.instagram.com/knowathens/'/>
                 <WorkBox company='Toyota Financial Services' title='Information & Digital Solutions Intern' time='May 2023 - August 2023' website='https://www.toyotafinancial.com/us/en.html'/>
             </div>
